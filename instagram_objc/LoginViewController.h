1d305418aee6bba1d97094fa208bda1a1ad63324
Makayla <makrodriguez@csumb.edu>
2022-05-31 13:50:50 -0700

    added login View controller

---
+++ instagram_objc/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  instagram_objc
+//
+//  Created by Makayla Rodriguez on 5/31/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
